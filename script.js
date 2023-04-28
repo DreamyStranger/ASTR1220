9f9a1ddd431b89303826ce428ea057e4a598e1c6
DreamyStranger <98614364+DreamyStranger@users.noreply.github.com>
2023-04-27 22:10:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
+    // Fade-in effect on page load
+    $('body').css('opacity', 0).animate({ opacity: 1 }, 200);
+
     $('.page-transition-link').on('click', function (e) {
-      e.preventDefault();
-      var targetUrl = $(this).data('href');
-  
-      // Slide the white rectangle out of view
-      $('.white-rectangle').css('transform', 'translateX(-100%)');
-  
-      // Wait for the animation to finish and then navigate to the target URL
-      setTimeout(function () {
-        window.location.href = targetUrl;
-      }, 500);
+        e.preventDefault();
+        var targetUrl = $(this).data('href');
+
+        // Fade-out effect before navigating to the next page
+        $('body').animate({ opacity: 0 }, 200, function () {
+            window.location.href = targetUrl;
+        });
     });
-  });
-  
+});
